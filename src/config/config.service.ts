65d999f854f8e5ae9f1382444b09e549a62f7bd0
ismael-vargas <iwz.vargas@yavirac.edu.ec>
2024-11-13 10:27:32 -0500

    config arreglado

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -5,19 +5,20 @@ import { parse } from 'dotenv';
 @Injectable()
 export class ConfigService {
   private readonly envConfig: { [key: string]: string };
-  
+
   constructor() {
     const env = process.env.NODE_ENV || 'development';
-    const envFilePath = `${__dirname}/../../.env.${env}`;
+    const envFilePath = `${process.cwd()}/.env.${env}`; 
+    //console.log(`Buscando archivo de entorno en: ${envFilePath}`);
+    
     const existsPath = fs.existsSync(envFilePath);
-
     if (!existsPath) {
-      console.log(`.env.${process.env.NODE_ENV} no existe`);
+      console.error(`Archivo .env.${env} no existe en la ruta: ${envFilePath}`);
       process.exit(0);
     }
 
     this.envConfig = parse(fs.readFileSync(envFilePath));
-    // console.log("**********", envFilePath)
+    //console.log(`Variables de entorno cargadas:`, this.envConfig);
   }
 
   get(key: string): string {
